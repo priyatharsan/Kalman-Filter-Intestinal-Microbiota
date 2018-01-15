2885c2239194fc04f7903fb0fc3d32076fd7b2a5
Ruiqi <ruiqi.zhong@gatech.edu>
2018-01-15 10:13:14 -0500

    delete old utils script

--- utils.py
+++ utils.py
@@ -157,3 +157,50 @@ def pad_with_most_recent_measurements(X):
             X_single_seq.append(np.array(most_recent_measurement))
         X_padded.append(X_single_seq)
     return X_padded
+
+
+def write_otu(X, otu_file, idx2bacteria, id2start_date):
+    """
+    Write X to the otu_file in the otu table format
+
+    Parameters
+    ----------
+    X: [seq_count] array of [seq_length] array of ([dimension] numpy array)
+        'X[seq_idx][time_step]' = the (inferred) frequency of the seq_idx sequence at time time_step
+    otu_file: String
+        the file name of the otu Table file
+    idx2bacteria: an array of String
+        a map from a dimension in the numpy array to its corresponding 'bacteria cluster'
+    id2start_date: {int: int}
+        a map from patientId (starting from 0) to the starting measurement date
+        relative to the reference date
+    """
+    out_file = open(otu_file, 'w')
+
+    # write the patient id line
+    id_line = 'patientId\t'
+    for _ in range(len(X)):
+        id = _ + 1
+        id_line += (str(id) + '\t') * len(X[_])
+    id_line += '\n'
+    out_file.write(id_line)
+
+    # write the time line
+    time_line = 'time\t'
+    for _ in range(len(X)):
+        start_date = id2start_date[_]
+        for time_step in range(len(X[_])):
+            time_line += str(start_date + time_step) + '\t'
+    time_line += '\n'
+    out_file.write(time_line)
+
+    # write the microbiiome
+    for idx in range(len(idx2bacteria)):
+        bacertia_name = idx2bacteria[idx]
+        rel_abundance_line = bacertia_name + '\t'
+        for _ in X:
+            for __ in _:
+                rel_abundance_line += str(__[idx]) + '\t'
+        rel_abundance_line += '\n'
+        out_file.write(rel_abundance_line)
+    out_file.close()
